74f9a798056fe4452571a38633c63300425ac785
Xwilarg <xwilarg@protonmail.com>
2025-04-05 13:50:11 +0200

    Add jumper AI

--- Assets/Scripts/Enemy/JumperEnemy.cs
+++ Assets/Scripts/Enemy/JumperEnemy.cs
@@ -5,6 +5,8 @@ namespace LudumDare57.Enemy
 {
     public class JumperEnemy : AEnemy
     {
+        private const float MaxX = 10f;
+
         public override void DoAction()
         {
             var target = PlayerManager.Instance.GetClosest(transform.position);
@@ -14,7 +16,9 @@ namespace LudumDare57.Enemy
                 return;
             }
 
-
+            var xDiff = target.transform.position.x - transform.position.x;
+            var xDir = Mathf.Clamp(xDiff, -MaxX, MaxX);
+            _rb.AddForce(new Vector2(xDir, 3f * MaxX / 4f).normalized * 30f, ForceMode2D.Impulse);
         }
     }
 }
