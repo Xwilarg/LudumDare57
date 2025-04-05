32a65b3cd83f91c9606be9226d6ce1a1fa027539
Xwilarg <xwilarg@protonmail.com>
2025-04-05 18:15:26 +0200

    Add bouncer enemy AI

---
+++ Assets/Scripts/Enemy/BouncerEnemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace LudumDare57.Enemy
+{
+    public class BouncerEnemy : AEnemy
+    {
+        private Vector2 _baseVelocity;
+
+        public override void DoAction()
+        {
+            _baseVelocity = Vector2.ClampMagnitude(_rb.linearVelocity + ((Vector2)(_target.transform.position - transform.position)).normalized * 5f, 7.5f);
+            _rb.linearVelocity = _baseVelocity;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            _rb.linearVelocity = Vector2.Reflect(_baseVelocity, collision.contacts[0].normal).normalized * _baseVelocity.magnitude;
+        }
+    }
+}
